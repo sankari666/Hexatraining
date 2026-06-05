2fa0aa0500a712bc6adb7952e8059a70d0e97f2f
sankari666 <148035324+sankari666@users.noreply.github.com>
2026-06-05 20:12:35 +0530

    Update Exception Handling.py

--- June - 5/Exception Handling.py
+++ June - 5/Exception Handling.py
@@ -1,1 +1,50 @@
+#general exceptional handling
+try:
+    a=10
+    b=0
+    result=a/b
+    print(result)
+except:
+    print("error occured")
+print("Program completed")
 
+#specific exceptional handling
+try:
+    a=10
+    b=0
+    result=a/b
+    print(result)
+except ZeroDivisionError:
+    print("Cannot divide by 0")
+
+try:
+    age=int(input("Enter age: "))
+    print(age)
+except ValueError:
+    print("Please enter a numeric value")
+
+try:
+    age=int(input("Enter age: "))
+    print(100/age)
+except ValueError:
+    print("Invalid number")
+except ZeroDivisionError:
+    print("Age cannot be zero")
+
+#Exception Object
+try:
+    num=int("abc")
+except Exception as e:
+    print(e)
+
+try:
+    print(10/0)
+except:
+    print("Error")
+finally:
+    print("Connection Closed")
+
+#raise error
+salary=-1000
+if salary<0:
+    raise ValueError('Salary cannot be negative')
