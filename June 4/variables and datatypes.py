9ac011d62d15ab099281baaf7eceb343e06d1a34
sankari666 <148035324+sankari666@users.noreply.github.com>
2026-06-05 10:04:26 +0530

    Update variables and datatypes.py

--- June 4/variables and datatypes.py
+++ June 4/variables and datatypes.py
@@ -1,1 +1,25 @@
+# OPERATORS
 
+#AND
+experience=5
+salary=80000
+
+if experience>=3 and salary>=50000:
+    print("Eligible")
+else:
+    print("Not Eligible")
+
+experience=1
+salary=50000
+
+#OR
+if experience>=3 or salary>=50000:
+    print("Eligible")
+else:
+    print("Not Eligible")
+
+#NOT
+is_blocked=False
+
+if not is_blocked:
+    print("Login Allowed")
