c72cbcb802b241d099de864f2c630f8f3b26c082
sankari666 <148035324+sankari666@users.noreply.github.com>
2026-06-05 20:16:12 +0530

    Create numpy.py

---
+++ June - 5/numpy.py
@@ -0,0 +1,24 @@
+#numpy
+import numpy as np
+arr=np.array([1,2,3,4,5])
+print(arr)
+print(arr+5)
+print(arr*2)
+print(np.sum(arr))
+print(np.mean(arr))
+print(np.max(arr))
+print(np.min(arr))
+print(arr.shape)
+
+#2D array
+arr=np.array([10,20,30],[40,50,60])
+print(arr)
+
+arr=np.zeros((3,4))
+print(arr)
+
+arr=np.ones((2,3))
+print(arr)
+
+arr.arange(1,11)
+print(arr)
