1193d4593087d0f2cb1f09da48108224346738bc
sankari666 <148035324+sankari666@users.noreply.github.com>
2026-06-05 10:03:13 +0530

    Update Conditional_statements.py

--- June 4/Conditional_statements.py
+++ June 4/Conditional_statements.py
@@ -1,1 +1,28 @@
+salary=35000
 
+if salary>50000:
+    print("High salary")
+else:
+    print("Low salary")
+
+# Multiple statements
+
+salary=75000
+experience=5
+if salary>=50000 and experience>=3:
+    print("Eligible")
+else:
+    print("Not Eligible")
+
+# IF ELSE
+
+marks=86
+
+if marks>=90:
+    print("Grade A")
+elif marks>=75:
+    print("Grade B")
+elif marks>=60:
+    print("Grade C")
+else:
+    print("Grade D")
